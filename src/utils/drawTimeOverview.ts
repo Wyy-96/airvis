c2ff2fb188f5af9c424a00fc0ed18a33968819f5
Wyy-96 <ping_o96@qq.com>
2021-06-04 19:38:32 +0800

    master

--- src/utils/drawTimeOverview.ts
+++ src/utils/drawTimeOverview.ts
@@ -20,34 +20,90 @@ export function drawLineOverview(): void {
       .attr("width", config.width)
       .attr("height", config.height);
 
-    var rect = svg.append("g").selectAll("rect")
+
+    const backgroundColor = ["#ADC965", "#89D5C9", "#FAC172", "#FF8357", "#E25B45", "#6CAC9C"]
+
+    svg.append("g")
+      .selectAll("rect")
       .data(data)
       .join("rect")
-      .attr("class", (d: any) => "day" + d.day)
       .attr("height", config.height)
-      .attr("width", config.width / 186)
-      .attr("x", (d, i) => config.width / 31 / 6 * i)
-      .attr("fill", (d, i) => "#000000");
-    var y_stack = 0.65; //每个叉与前一个叉的y轴上的位移的参数
-    var day = svg.selectAll("g")
+      .attr("width", config.width / 6)
+      .attr("transform", (d, i) => `translate(${config.width / 6 * i},0)`)
+      .attr("fill", (d, i) => backgroundColor[i])
+    var yearRect = svg.append("g").selectAll("g")
       .data(data)
       .join("g")
-      .attr("transform", (d, i) => `translate(${config.width / 31 / 6 * i},0)`);
-    day.selectAll("polyline")
-      .data((d:any) => d.data)
+      .attr("fill", "none")
+      .attr("stroke", "#000000")
+      .attr("transform", (d, i) => `translate(${config.width / 6 * i},0)`)
+    var dayRect = yearRect.selectAll("g")
+      .data((d: any) => d)
+      .join("g")
+      .attr("transform", (d, i) => `translate(${config.width / 6 / 31 * i},0)`)
+
+    var y_stack = 5; //每个叉与前一个叉的y轴上的位移的参数
+    var x_stack = config.width / 31 / 6 / 24
+
+    dayRect.selectAll("polyline")
+      .data((d: any) => d.data)
       .join("polyline")
-      .attr("points", "0,0 7.5,10 3.75,5 0,10 7.5,0 3.75,5")
+      .attr("points", "0,0 6,4 3,2 0,4 6,0")
       .attr("stroke", "white")
-      .attr("transform", (d:any, i) => `translate(0,${y_stack * d.id})`)
-    day.selectAll("line")
-      .data((d:any) => d.data)
+      .attr("transform", (d: any, i) => `translate(${x_stack * getTail(d.start)},${i * y_stack})`)
+      .attr("opacity", 0.9)
+
+    dayRect.selectAll("line")
+      .data((d: any) => d.data)
       .join("line")
-      .attr("x1", 7.5)
-      .attr("y1", 5)
-      .attr("x2", (d:any) => (d.duration / 24) * 7.5 + 3.75)
-      .attr("y2", 5)
+      .attr("x1", 6)
+      .attr("y1", 2)
+      .attr("x2", (d: any) => (d.duration / 24) * 3 + 6)
+      .attr("y2", 2)
       .attr("stroke", "white")
-      .attr("transform", (d:any, i) => `translate(0,${y_stack * d.id})`)
+      .attr("transform", (d: any, i) => `translate(${x_stack * getTail(d.start)},${i * y_stack})`)
+
+    yearRect.selectAll("rect")
+      .data((d: any) => d)
+      .join("rect")
+      .attr("height", config.height)
+      .attr("width", config.width / 31)
+      .attr("x", (d, i) => config.width / 31 * i)
+      .attr("stroke", "none")
+      
+    function getTail(d: string) {
+      let n = parseInt(d.substring(d.length - 2))
+      return n
+    }
+
+    // var rect = svg.append("g").selectAll("rect")
+    //   .data(data)
+    //   .join("rect")
+    //   .attr("class", (d: any) => "day" + d.day)
+    //   .attr("height", config.height)
+    //   .attr("width", config.width / 186)
+    //   .attr("x", (d, i) => config.width / 31 / 6 * i)
+    //   .attr("fill", (d, i) => "#000000");
+
+    // var day = svg.selectAll("g")
+    //   .data(data)
+    //   .join("g")
+    //   .attr("transform", (d, i) => `translate(${config.width / 31 / 6 * i},0)`);
+    // day.selectAll("polyline")
+    //   .data((d:any) => d.data)
+    //   .join("polyline")
+    //   .attr("points", "0,0 7.5,10 3.75,5 0,10 7.5,0 3.75,5")
+    //   .attr("stroke", "white")
+    //   .attr("transform", (d:any, i) => `translate(0,${y_stack * d.id})`)
+    // day.selectAll("line")
+    //   .data((d:any) => d.data)
+    //   .join("line")
+    //   .attr("x1", 7.5)
+    //   .attr("y1", 5)
+    //   .attr("x2", (d:any) => (d.duration / 24) * 7.5 + 3.75)
+    //   .attr("y2", 5)
+    //   .attr("stroke", "white")
+    //   .attr("transform", (d:any, i) => `translate(0,${y_stack * d.id})`)
   }
 
 
