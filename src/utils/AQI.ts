b905973764b7ba8ba3233c96aa520a02809ab232
Wyy-96 <ping_o96@qq.com>
2021-06-08 17:49:51 +0800

    master

--- src/utils/AQI.ts
+++ src/utils/AQI.ts
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
 import axios from "axios"
 import store from "@/store";
+import { packSiblings } from 'd3';
 
 export function drawAQIMap(ymd: any, hour: any): void {
   // axios({
@@ -27,16 +28,40 @@ export function drawAQIMap(ymd: any, hour: any): void {
       width: parseInt(divMap.style("width")) - 2,
       height: parseInt(divMap.style("height")) - 2,
     }
-    const svg = divMap.select("div").append("svg")
+    const svg = divMap.select("div").append("svg") //select("div").
       .attr("width", config.width)
       .attr("height", config.height)
+
     const mapG = svg.append("g")
 
     var projection = d3.geoMercator() //投影
-      .center([121, 38])
-      .translate([500, 400])
-      .scale(500)
+      .center([102, 36])
+      .translate([450, 330])
+      .scale(750);
 
-    
+    var colorIndex = d3.scaleLinear([0, 50, 100, 150, 200, 300, 501],["#5BD665", "#F7F452", "#FD9813", "#F5461A", "#7E0303", "#330101", "#130000"])
+
+
+    const color = ["#5BD665", "#F7F452", "#FD9813", "#F5461A", "#7E0303", "#330101", "#130000"]
+    var location = mapG.selectAll(".location")   //根据经纬度坐标coor的位置添加g元素
+      .data(data)
+      .join("g")
+      .attr("class", "location")
+      .attr("transform", function (d: any) {
+        //计算标注点的位置
+        var coor: any = projection([d["lon"], d["lat"]]);
+        return "translate(" + coor[0] + "," + coor[1] + ")";
+      });
+
+    location.append("rect")   //在g元素中加一个rect，调整合适的角度位置
+      .attr("width", 1.6)
+      .attr("height", 1.6)
+      .attr("transform", `rotate(${0}) translate(-0.8,-0.8)`)
+      .attr("fill", (d:any) => {
+        // console.log(colorIndex(parseFloat(d["AQI"])))
+        return colorIndex(parseFloat(d["AQI"]))
+      })
+    //.attr("transform", "translate(, 20)")
+
   }
 }
